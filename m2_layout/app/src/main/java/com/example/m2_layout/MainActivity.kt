5366a2cb2bad1b93350f40a19bb0ce9a009401e9
Чекус Евгений <148764280+Milteng@users.noreply.github.com>
2024-08-13 21:07:12 +0300

    Commit m2

---
+++ m2_layout/app/src/main/java/com/example/m2_layout/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.m2_layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.m2_layout.ui.theme.M2_layoutTheme
+import com.example.m2_layout.databinding.ActivityMainBinding
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.text1.text = getText(R.string.text1_code);
+        binding.text2.text = getText(R.string.text2_code);
+
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    M2_layoutTheme {
+        Greeting("Android")
+    }
+}
