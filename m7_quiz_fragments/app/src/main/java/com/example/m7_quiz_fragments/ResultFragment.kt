287a1a15f17b15d62360659f4d74c885e81f05c7
Чекус Евгений <148764280+Milteng@users.noreply.github.com>
2024-08-31 09:18:15 +0300

    m7

---
+++ m7_quiz_fragments/app/src/main/java/com/example/m7_quiz_fragments/ResultFragment.kt
@@ -0,0 +1,59 @@
+package com.example.m7_quiz_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.m7_quiz_fragments.databinding.FragmentResultBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "number"
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ResultFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ResultFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    private var param1: Int? = null
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getInt(ARG_PARAM1)
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentResultBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.result.text = param1.toString() + "/3"
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_resultFragment_to_quizFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
---
+++ m7_quiz_fragments/app/src/main/java/com/example/m7_quiz_fragments/WelcomeFragment.kt
@@ -0,0 +1,84 @@
+package com.example.m7_quiz_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.example.m7_quiz_fragments.databinding.FragmentWelcomeBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WelcomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WelcomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    //переход на фрагмент с рузультатами и передача параметра количества правильных ответов
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_quizFragment)
+
+            ;
+            //val action = QuizFragmentDirections.actionQuizFragmentToResultFragment(number)
+            //findNavController().navigate(action)
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWelcomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment welkomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            WelcomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
