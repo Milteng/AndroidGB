5031bc7070c6c4b0ad7c9973923969cf7475faa2
Чекус Евгений <148764280+Milteng@users.noreply.github.com>
2024-09-03 21:16:58 +0300

    m8

---
+++ m8_quiz_animation/app/src/main/java/com/example/m7_quiz_fragments/QuizFragment.kt
@@ -0,0 +1,114 @@
+package com.example.m7_quiz_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.m7_quiz_fragments.databinding.FragmentQuizBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [QuizFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class QuizFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var _binding: FragmentQuizBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentQuizBinding.inflate(inflater)
+        return binding.root
+    }
+
+    //переход на фрагмент с рузультатами и передача параметра количества правильных ответов
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        alphaRG(binding.question1)
+        alphaRG(binding.question2)
+        alphaRG(binding.question3)
+
+
+        binding.button.setOnClickListener {
+            val number = getCorrectAnswersByUser()
+            var bundle = bundleOf(ARG_PARAM_NUM to number)
+            findNavController().navigate(R.id.action_quizFragment_to_resultFragment, bundle)
+
+                ;
+            //val action = QuizFragmentDirections.actionQuizFragmentToResultFragment(number)
+            //findNavController().navigate(action)
+
+        }
+    }
+
+    fun alphaRG(radioGroup: RadioGroup){
+        radioGroup.alpha = 0.1f;
+        radioGroup.animate().apply{
+            duration = 3000;
+            alpha(1f)
+        }.start()
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    //счетчик правильных ответов
+    private fun getCorrectAnswersByUser(): Int {
+        var correctAnswersCount = 0
+
+        if (binding.question1.checkedRadioButtonId == binding.answer12.id) correctAnswersCount++
+        if (binding.question2.checkedRadioButtonId == binding.answer22.id) correctAnswersCount++
+        if (binding.question3.checkedRadioButtonId == binding.answer32.id) correctAnswersCount++
+
+        return correctAnswersCount
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment QuizFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            QuizFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
