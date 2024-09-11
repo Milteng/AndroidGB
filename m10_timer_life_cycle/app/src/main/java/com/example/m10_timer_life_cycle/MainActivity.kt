32cb779d7c833f9d12e26bf834c2ff9363edbcc1
Чекус Евгений <148764280+Milteng@users.noreply.github.com>
2024-09-11 17:03:24 +0300

    m10

--- m10_timer_life_cycle/app/src/main/java/com/example/m10_timer_life_cycle/MainActivity.kt
+++ m10_timer_life_cycle/app/src/main/java/com/example/m10_timer_life_cycle/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.m10_timer_life_cycle
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.m10_timer_life_cycle.databinding.ActivityMainBinding
 
@@ -14,49 +16,47 @@ private const val c_timerIsActive = "timerIsActive"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var handler = Handler(Looper.getMainLooper()) //для синхронизации с онсовным потоком
+    private var handler = Handler(Looper.getMainLooper())
 
-    private var timerValue = 10 // значение таймера
-    private var timeCounter = 10 // количество по секундам
-    private var timerIsActive = false // активность таймера
-    private var timerThreadStop = false // флаг остановки потока
+    private var timerValue = 10
+    private var timeCounter = 10
+    private var timerIsActive = false
 
-    private lateinit var timerThread: Thread // поток таймера
-
-
+    private lateinit var timerThread: Thread
+    private var timerThreadStop = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.slider.addOnChangeListener { _, value, _ ->
-            timerValue = value.toInt()
-            timeCounter = timerValue
-            updateEnableIFace()
-        }
-
-        binding.buttonStart.setOnClickListener {
-            timerExecute()
-        }
-
-        //если восстанавливаем работу активити
         if (savedInstanceState != null) {
             timerValue = savedInstanceState.getInt(c_timerValue)
             timerIsActive = savedInstanceState.getBoolean(c_timerIsActive)
+            Log.d("msg", "onCreate: после изменения $timerValue")
             timeCounter = timerValue
-
-            updateTimer()
-
-            if (timerIsActive) timerExecute()
+            handler.post {
+                updateTimer()
+                if (timerIsActive) startTimer()
+            }
         }
 
+        handler.post {
+            binding.slider.addOnChangeListener { _, value, _ ->
+                timerValue = value.toInt()
+                timeCounter = timerValue
+                updateUI()
+            }
+
+            binding.buttonStart.setOnClickListener {
+                startTimer()
+            }
+        }
     }
 
-    //функция обновления состояния элементов интерфейса
-    private fun updateEnableIFace() {
+    private fun updateUI() {
         when (timerIsActive) {
-            true -> { //если поток таймера активен, не сбрасываем активность элементов
+            true -> {
                 binding.slider.isEnabled = false
                 binding.buttonStart.visibility = View.GONE
                 binding.buttonStop.visibility = View.VISIBLE
@@ -73,64 +73,64 @@ class MainActivity : AppCompatActivity() {
         binding.timer.text = timerValue.toString()
     }
 
-    //функция обновления по значению таймера
     private fun updateTimer() {
         binding.timer.text = timeCounter.toString()
         binding.progressBar.progress = timeCounter
     }
 
-
-
-    fun timerExecute() {
-        timerThread = Thread(){
+    private fun startTimer() {
+        timerThread = Thread {
             while (!timerThreadStop && timeCounter > 0) {
-                Thread.sleep(1000) //делаем задержку в секунду
-                //навесим обработчик клика для завершения потока
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
 
                 binding.buttonStop.setOnClickListener {
                     timerIsActive = false
                     stopTimerThread()
                 }
-                //уменьшили счетчик
+
                 timeCounter--
-                //нужно отобразить все это дело в интерфейсе
+
                 handler.post {
                     updateTimer()
                 }
-
-
-
             }
-
             if (timeCounter == 0) timerIsActive = false
-            //сюда дошли по идее если цикл завершился
-            //восстановим значения
-            handler.post{   timerValue = binding.slider.value.toInt()
-                            timeCounter = timerValue
-                            timerThreadStop = false
-                            timerIsActive = false
-                            updateEnableIFace()
-            }
+            handler.post { finishTimer() }
         }
 
-
-        timerIsActive = true;
-        handler.post{updateEnableIFace()}
+        timerIsActive = true
+        handler.post { updateUI() }
         timerThread.start()
+    }
 
+    private fun finishTimer() {
+        if (!timerIsActive) {
+            Toast.makeText(this, "Timer Task Finished", Toast.LENGTH_SHORT).show()
+        }
+        timerValue = binding.slider.value.toInt()
+        timeCounter = timerValue
+        timerThreadStop = false
+        handler.post { updateUI() }
     }
 
     private fun stopTimerThread() {
         timerThreadStop = true
-        //timerThread.interrupt()
+        timerThread.interrupt()
     }
 
     override fun onPause() {
         super.onPause()
-        stopTimerThread()
+        try {
+            stopTimerThread()
+        } catch (ex: Exception) {
+            Log.d("msg", "onPause: $ex")
+        }
     }
 
-   //сохраняем значения переменных для возобновления потока после восстановления активити
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putInt(c_timerValue, timeCounter)
         outState.putBoolean(c_timerIsActive, timerIsActive)
