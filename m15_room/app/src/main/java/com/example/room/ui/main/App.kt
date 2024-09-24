6cfc615414591d2fa4102b26b74eb5ed724686f8
Чекус Евгений <148764280+Milteng@users.noreply.github.com>
2024-09-24 13:25:20 +0300

    m15

---
+++ m15_room/app/src/main/java/com/example/room/ui/main/App.kt
@@ -0,0 +1,20 @@
+package com.example.room.ui.main
+
+import android.app.Application
+import androidx.room.Room
+
+class App : Application() {
+    lateinit var db: WordsDatabase
+
+    override fun onCreate() {
+        super.onCreate()
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            WordsDatabase::class.java,
+            "wordsDb2.db"
+        )
+            //.addMigrations(MIGRATION_1_2)
+            .build()
+    }
+}
